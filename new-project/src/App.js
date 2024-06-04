839e3236c014e0493e7b2f383c76b24fa84a29d2
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-04 11:36:29 -0500

    yes

---
+++ new-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Navbar'
+import Main from './components/Main'
+
+function App() {
+  return (
+    <>
+        <Header />
+        <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ new-project/src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <header>
+            <nav>
+                <img src="logo192.png" alt="logo of react" />
+                <h3>ReactFacts</h3>
+                <p>React Course - Project 1</p>
+            </nav>
+        </header>
+    )
+}
